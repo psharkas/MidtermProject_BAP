6fd5422e44ae4afefe5c51458d1cc015ebb9c061
Bolanle <bola0321@gmail.com>
2022-05-25 12:10:11 -0400

    Member c abstract class, constructor, getter/setters, checkin abstract method

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,29 @@
 
-public class Member {
+abstract class Member {
+	private String name;
+	private int id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void printMemberInfo() {
+		System.out.println("Member ID: " + id);
+		System.out.println("Member name: " + name);
+	}
+
+	public abstract void checkIn(Club club);
 
 }
