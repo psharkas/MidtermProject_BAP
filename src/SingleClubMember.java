9a5d67b0e3faf60ff62cc5628fa6dcacc54d3b27
alexis <asimpson428@gmail.com>
2022-05-26 19:16:35 -0400

    day 3

--- src/SingleClubMember.java
+++ src/SingleClubMember.java
@@ -1,14 +1,53 @@
 
 public class SingleClubMember extends Member{
 	
-	public String iD;
-	public String name;
+	public SingleClubMember(String name, int id) {
+		super(name, id);
+		// TODO Auto-generated constructor stub
+	}
+
 	public int clubNum;
-	//do we want to add secondary ID in case two members have same name?
 	
-	 singleClub single = new singleClub (id, name, clubNum) 
+	String SingleClub single = new SingleClub(id,name,Club);
+	
+	Club locationA = new Club();
+	Club locationB = new Club();
+	Club locationC = new Club();
+	Club locationD = new Club();
+	
+	public int trackClub (String name, int id) {
+		super();
+		this.getName();
+		this.getId();
+		this.setClubNum(getClubNum());
+		return clubNum;
+	}
+	
+	public String correctClub ( String name, int id, int clubNum) {
+		//conditional statement to show if assigned club number is being used.
+		return null;
+	}
+
+
+	@Override
+	public void checkIn(Club club) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getClubNum() {
+		return clubNum;
+	}
+
+	public void setClubNum(int clubNum) {
+		this.clubNum = clubNum;
+	} 
 			 
 	//track the club member is assigned to
+	
+	//do we want to add secondary ID in case two members have same name?
+	
+	 //singleClub single = new singleClub (id, name, clubNum)
 			 
 	//When member is checked in confirm club is assigned club
 	//loop through info to check assigned club
@@ -16,6 +55,6 @@ public class SingleClubMember extends Member{
 
 	
 		
-	}
+	
 
 }
