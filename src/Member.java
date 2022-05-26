b51c9a89f1a318e9b2b2fdfaa46e701df9b2c35f
psharkas <petersharkas7@gmail.com>
2022-05-25 21:04:13 -0400

    Added Bill of Fees class and some more stuff to Club

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,35 @@
 
-public class Member {
+abstract class Member {
+	private String name;
+	private int id;
+	
+	public Member (String name, int id) {
+		this.name = name;
+		this.id = id;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void printMemberInfo() {
+		System.out.println("Member ID: " + id);
+		System.out.println("Member name: " + name);
+	}
+
+	public abstract void checkIn(Club club);
 
 }
