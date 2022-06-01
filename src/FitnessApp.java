affd46bdf76644e68c7011b5a414c599adb28c80
Bolanle <bola0321@gmail.com>
2022-06-01 17:11:47 -0400

    added some dashes for reading clarity and more comments to my code

--- src/FitnessApp.java
+++ src/FitnessApp.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class FitnessApp {
 
 	public static void main(String[] args) {
+		//hard-coded arraylist called into the program
 		ArrayList<Member> memberList = initializeMemberList();
 
 		Scanner scan = new Scanner(System.in);
@@ -16,6 +17,7 @@ public class FitnessApp {
 		System.out.println("What would you like to do?" + "\n");
 
 		while (true) {
+			//ask user if they want to create new member or access existing member
 			chooseMemberMenu();
 			int userEntryChooseMemberMenu = promptUserForInt(scan, 3);
 			int indexOfMember;
@@ -23,19 +25,22 @@ public class FitnessApp {
 			if (userEntryChooseMemberMenu == 1) {
 				// create new member
 			} else if (userEntryChooseMemberMenu == 2) {
-
+				//prompt user to enter either an id num or name, then stores the index where it is found in numOrID.
+				dashes(50);
 				indentPrintString(1, "1. Enter your name");
 				indentPrintString(1, "2. Enter your ID number");
 				int numOrID = promptUserForInt(scan, 2);
 
 				if (numOrID == 1) {
 					System.out.print("Please enter user name here: ");
+					System.out.println("");
 					String userNameEntry = scan.nextLine();
 					indexOfMember = findMemberByName(memberList, userNameEntry);
 
 				} else {
 					System.out.print("Please enter user ID here: ");
 					int userIDentry = scan.nextInt();
+					System.out.println("");
 					indexOfMember = findMemberById(memberList, userIDentry);
 				}
 				if (indexOfMember == -1) {
@@ -43,12 +48,10 @@ public class FitnessApp {
 					System.out.println("Please check again by entering your ID number or feel free to become a member of BAP Fitness!");
 					continue;
 				}
-				// TODO: method that asks user their name or id number, store index in variable
-
+				//display menu for checking in, user info and billing, and canceling membership. Get user int to go into switch
 				existingMemberMenu();
-				promptMenuChoice();
-
-				int userEntryExistingMemberMenu = scan.nextInt();
+				int userEntryExistingMemberMenu = promptUserForInt(scan,3);
+				
 
 				// TODO: put this switch in a method for ease of reading main
 				switch (userEntryExistingMemberMenu) {
@@ -56,13 +59,14 @@ public class FitnessApp {
 
 					break;
 				case 2:
+					//Prints out member info and billing 
 					System.out.println("");
 					BillOfFees.getFee(memberList.get(indexOfMember));
 					System.out.println("");
 
 					break;
-				case 3:// ask user to confirm deletion again if yes, delete. if no, exit or
-						// something.... BOLANLE
+				case 3://ask you user if they really want to cancel, if yes, then removes member from arraylist
+						
 					indentPrintString(5, "ARE  YOU SURE YOU WANT TO CANCEL YOUR MEMBERSHIP?");
 					indentPrintString(5, "1. Yes");
 					indentPrintString(5, "2. No");
@@ -71,6 +75,10 @@ public class FitnessApp {
 						System.out.println(memberList.get(indexOfMember).getName() + "has been removed from BAP Fitness. We're sorry to see you go!");
 						memberList.remove(indexOfMember);
 						continue;
+					}else if (deleteYN == 2) {
+						System.out.println("");
+						System.out.println("We're glad you're staying with us!");
+						System.out.println();
 					}
 					
 					break;
@@ -101,13 +109,18 @@ public class FitnessApp {
 		System.out.println("");
 	}
 
+	
+	//menu for existing 
 	public static void chooseMemberMenu() {
+		dashes(50);
 		indentPrintString(1, "1. Create new member");
 		indentPrintString(1, "2. Access existing member");
-		indentPrintString(1, "Exit the program");
+		indentPrintString(1, "3. Exit the program");
 	}
-
+	
+	//menu for existing members
 	public static void existingMemberMenu() {
+		dashes(50);
 		indentPrintString(3, "1. Check-in!");
 		indentPrintString(3, "2. See your membership information and billing");
 		indentPrintString(3, "3. Cancel your membership with BAP Fitness");
@@ -117,6 +130,7 @@ public class FitnessApp {
 
 	}
 
+	//prints a string with desired format indentation
 	public static void indentPrintString(int indentLevel, String str) {
 		System.out.printf("%" + indentLevel + "s", "");
 		System.out.printf(str + "\n");
@@ -129,8 +143,11 @@ public class FitnessApp {
 	public static void currentMemberSwitch(int x, Scanner scan) {
 		// TODO PLACE SWITCH HERE
 	}
-
+	
+	
+	//hard-coded arraylist
 	public static ArrayList<Member> initializeMemberList() {
+		
 		ArrayList<Member> memberList = new ArrayList<Member>();
 		memberList.add(new MultipleClubMember("Brian Smith", 457299));
 		memberList.add(new MultipleClubMember("Booboo Thefool", 123456));
@@ -138,9 +155,10 @@ public class FitnessApp {
 		memberList.add(new SingleClubMember("Who Dunnit", 678901, null));
 
 		return memberList;
-
 	}
-
+	
+	
+	//prompts user to select menu option. Has a range set (1 to maxNum) and loops until it gets num in that range
 	public static int promptUserForInt(Scanner scan, int maxNum) {
 		int heresANum;
 		do {
@@ -153,17 +171,7 @@ public class FitnessApp {
 
 	}
 
-	public static int promptUserForString(Scanner scan, int indexLevel, String prompt) {
-		// Print out the prompt, loop until the user gives you a non-empty string.
-		return 0;
-	}
-
-	public static int promptUserToFindMember(Scanner scan, int indexLevel) {
-		// The idea is to encapsulate all of your logic around "do you want to find by
-		// name or id? enter name: etc"
-		return 0;
-	}
-
+	// gives method a list and a ID name and return the index of a member
 	public static int findMemberByName(ArrayList<Member> memberList, String memberName) {
 		for (int i = 0; i <= memberList.size() - 1; i++) {
 			if (memberList.get(i).getName().equals(memberName)) {
@@ -172,10 +180,11 @@ public class FitnessApp {
 		}
 		return -1;
 	}
-
+	
+	
+	// gives method a list and a ID number and return the index of a member
 	public static int findMemberById(ArrayList<Member> memberList, int ID) {
-		// The idea is to give it the list and a name and return the index of a member
-		// which has
+		
 		for (int i = 0; i <= memberList.size() - 1; i++) {
 			if (memberList.get(i).getId() == ID) {
 				return i;
